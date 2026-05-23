cdc7445f07c631c0d1ecb2cf7c7cdd08f45935d3
ryan.h.park <claudianus@engineer.com>
2026-05-23 14:43:12 +0900

    chore(release): v0.22.6

--- dist/src/version.d.ts
+++ dist/src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "0.22.5";
-export declare const USER_AGENT = "kakaotalk-chat-analyzer/0.22.5";
+export declare const VERSION = "0.22.6";
+export declare const USER_AGENT = "kakaotalk-chat-analyzer/0.22.6";
